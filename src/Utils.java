ca681b558c3d7995278fa07637e83d0beadb69cf
varun <varun.nealsri@gmail.com>
2019-03-03 23:44:44 -0800

    coded getDiff

--- src/Utils.java
+++ src/Utils.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Utils {
 
 
+
     public static String readFileAsString(String filepath) {
         StringBuilder output = new StringBuilder();
 
@@ -22,8 +23,36 @@ public class Utils {
         return output.toString();
     }
 
+
+    /***
+     *   private double votesDem, votesGOP, totalVotes, perDem, perGOP;
+     *     private int !diff;
+     *     private double perPointDiff;
+     *     private String stateAbbr, countyName;
+     *     private int combinedFips;
+     */
+
+
     public static ArrayList<ElectionResult> parse2016PresidentialResults(String csv_data) {
+        String[] lines = csv_data.split("\n");
+        ArrayList<ElectionResult> out = new ArrayList<>();
+
+        for (int i = 1; i < lines.length; i++) {
+            String current = lines[i];
+            ElectionResult temp = new ElectionResult();
 
 
+        }
+
+        return out;
+    }
+
+    private static int getDiff(String current) {
+        String[] temp = current.split("\"");
+        String diff = temp[1];
+        if ( diff.indexOf(",") != -1 ) {
+            int out = Integer.parseInt(diff.replace(",", ""));
+            return out;
+        } else return Integer.parseInt(diff);
     }
 }
