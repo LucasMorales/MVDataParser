1eb14b708d81358b8f2ea6c45f399c5446b27a74
varun <varun.nealsri@gmail.com>
2019-03-03 23:59:51 -0800

    filled out all helper methods in Utils

--- src/Utils.java
+++ src/Utils.java
@@ -58,31 +58,28 @@ public class Utils {
     }
 
     private static int getCombinedFips(String[] commaBreaks) {
+        return Integer.parseInt(commaBreaks[commaBreaks.length - 1]);
     }
 
-    private static String getCountyName(String[] commaBreaks) {
-    }
+    private static String getCountyName(String[] commaBreaks) { return commaBreaks[commaBreaks.length - 2]; }
 
-    private static String getStateAbbr(String[] commaBreaks) {
-    }
+    private static String getStateAbbr(String[] commaBreaks) { return commaBreaks[commaBreaks.length -3];}
 
     private static double getPerPointDiff(String[] commaBreaks) {
+        String temp = commaBreaks[commaBreaks.length - 4];
+        temp = temp.substring(0,temp.length() - 1);
+        return Double.parseDouble(temp);
     }
 
-    private static double getPerGop(String[] commaBreaks) {
-    }
+    private static double getPerGop(String[] commaBreaks) { return Double.parseDouble(commaBreaks[5]); }
 
-    private static double getPerDem(String[] commaBreaks) {
-    }
+    private static double getPerDem(String[] commaBreaks) { return Double.parseDouble(commaBreaks[4]); }
 
-    private static double getTotalVotes(String[] commaBreaks) {
-    }
+    private static double getTotalVotes(String[] commaBreaks) { return Double.parseDouble(commaBreaks[3]); }
 
-    private static double getVotesGop(String[] current) {
-    }
+    private static double getVotesGop(String[] commaBreaks) { return Double.parseDouble(commaBreaks[2]); }
 
-    private static double getVotesDem(String[] current) {
-    }
+    private static double getVotesDem(String[] commaBreaks) { return Double.parseDouble(commaBreaks[1]); }
 
     private static int getDiff(String current) {
         String[] temp = current.split("\"");
