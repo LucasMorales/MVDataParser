b5956c7edeb343f241e4a338510382a918148653
varun <varun.nealsri@gmail.com>
2019-03-03 22:39:56 -0800

    main displays data files

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
  * Main class for data parsers
  * @author: Varun Srivastava
  */
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // test data reading
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 
 }
