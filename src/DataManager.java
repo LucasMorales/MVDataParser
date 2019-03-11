ff61b2655a8407a53a406971a52b3e64be258d44
varun <varun.nealsri@gmail.com>
2019-03-10 21:34:43 -0700

    added methods to DataManager class

--- src/DataManager.java
+++ src/DataManager.java
@@ -4,6 +4,24 @@ public class DataManager {
 
     private List<State> states;
 
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
 
+
+    public void addState(State st) {
+        states.add(st);
+    }
+
+    public State getState(String stateName) {
+        for (State state : states) {
+            if (state.getName().equals(name)) return state;
+        }
+        return null;
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
 
 }
