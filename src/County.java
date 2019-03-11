74c7dee15527cc219f8a815e379e795e92541b8e
varun <varun.nealsri@gmail.com>
2019-03-10 21:41:05 -0700

    created County class

--- src/County.java
+++ src/County.java
@@ -1,2 +1,8 @@
 public class County {
+    private String name;
+    private int fips;
+    private Election2016 vote2016;
+    private Education2016 education2016;
+    private Employment2016 employment2016;
+
 }
