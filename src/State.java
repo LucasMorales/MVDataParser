ea9c913c1838d23e5b031f931ce807b710d3e188
varun <varun.nealsri@gmail.com>
2019-03-10 21:38:55 -0700

    added getters and setters

--- src/State.java
+++ src/State.java
@@ -10,5 +10,28 @@ public class State {
         this.counties = new ArrayList<>();
     }
 
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<County> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List<County> counties) {
+        this.counties = counties;
+    }
+
+    public County getCountyName() {
+        for (County county : counties) {
+            if (county.getName().equals(name)) return county;
+        }
+        return null;
+    }
 
 }
