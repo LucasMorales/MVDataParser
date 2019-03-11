61664f5b93a68402430db1896bbbcfd9abff96ac
varun <varun.nealsri@gmail.com>
2019-03-10 21:37:07 -0700

    added constructor

--- src/State.java
+++ src/State.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class State {
     private String name;
     private List<County> counties;
+
+    public State(String name) {
+        this.name = name;
+        this.counties = new ArrayList<>();
+    }
+
 
 }
