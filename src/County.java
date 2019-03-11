31459dd63caece73d521d25759ecf4b5979772f8
varun <varun.nealsri@gmail.com>
2019-03-10 21:41:39 -0700

    added constructor

--- src/County.java
+++ src/County.java
@@ -5,4 +5,10 @@ public class County {
     private Education2016 education2016;
     private Employment2016 employment2016;
 
+
+    public County(String name, int fips) {
+        this.name = name;
+        this.fips = fips;
+    }
+
 }
