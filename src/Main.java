c9e1b24ef8862db367219f690632209f8c296820
varun <varun.nealsri@gmail.com>
2019-03-28 00:05:00 -0700

    sorted list

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
  * Main class for data parsers
  * @author: Varun Srivastava
  */
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+
 
 public class Main {
 
@@ -24,28 +27,58 @@ public class Main {
         for (String name : stateAbrv.keySet()) {
             String abrv = stateAbrv.get(name);
             compliledData[counter][0] = name;
-            compliledData[counter][1] = stateEducation.get(name);
+
+            String edu = stateEducation.get(name);
+            String eduPercent = clean(edu);
+            compliledData[counter][1] = eduPercent;
+
             compliledData[counter][2] = stateTrump.get(name);
+
             String funding = stateFunding.get(name);
             String fundingName = cleanUp(funding);
             compliledData[counter][3] = fundingName;
             counter++;
         }
 
+        Arrays.sort(compliledData, Comparator.comparingDouble(a-> Double.parseDouble(cleanUp(a[3],3))));
 
-//        for (int i = 0; i < compliledData.length; i++) {
-//            for (int j = 0; j < compliledData[0].length; j++) {
-//                System.out.print(compliledData[i][j]);
-//                System.out.print(", ");
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < compliledData.length; i++) {
+            for (int j = 0; j < compliledData[0].length; j++) {
+                System.out.print(compliledData[i][j]);
+                System.out.print(", ");
+            }
+            System.out.println();
+        }
     }
+
+    private static void sort(String[][] arr) {
+        double prevVal = 0;
+        for (int i = 0; i < arr.length; i++) {
+            String str = arr[i][2];
+            str = cleanUp(str,2);
+            double val = Double.parseDouble(str);
+
+
+        }
+
+    }
+
+    private static String clean(String edu) {
+        String out = String.valueOf(100 - Double.parseDouble(edu));
+        return out + "%";
+    }
+
 
     private static String cleanUp(String str) {
         str = str.replaceAll(" ","");
+        return str;
+    }
+
+    private static String cleanUp(String str, int a) {
+        str = str.replaceAll(" ","");
         str  = str.replaceAll("\\$","");
         str = str.replaceAll("\"","");
+        str = str.replaceAll(",","");
         return str;
     }
 
