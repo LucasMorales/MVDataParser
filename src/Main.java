5c2a7bbefc42a6b3beda65fe05ba6d258eb617e4
varun <varun.nealsri@gmail.com>
2019-03-27 23:41:08 -0700

    finished parsing methods

--- src/Main.java
+++ src/Main.java
@@ -17,34 +17,37 @@ public class Main {
             String fundingData = Utils.readFileAsString("data/stateFundingByPupil.csv");
             String abrvData = Utils.readFileAsString("data/stateAbrvs.csv");
 
-            HashMap<String, String> stateEducation = Utils.parseCSV(eduData, 6, 2, 44);
-            HashMap<String, String> stateTrump = Utils.parseCSV(trumpData,0,0,6);
-            HashMap<String, String> stateFunding = Utils.parseCSV(fundingData, 0, 1,0);
-            HashMap<String, String> stateAbrv = Utils.parseCSV(abrvData,0,0,1);
+            HashMap<String, String> stateEducation = Utils.parseCSV(eduData, 4, 2, 44,47);
+            HashMap<String, String> stateTrump = Utils.parseCSV(trumpData,0,0,6,25);
+            HashMap<String, String> stateFunding = Utils.parseCSV(fundingData, 0, 0,1,1);
+            HashMap<String, String> stateAbrv = Utils.parseCSV(abrvData,0,0,1,1);
 
         for (String name : stateAbrv.keySet()) {
             String abrv = stateAbrv.get(name);
-            compliledData[counter][0] = abrv;
-            compliledData[counter][1] = stateEducation.get(abrv);
+            compliledData[counter][0] = name;
+            compliledData[counter][1] = stateEducation.get(name);
             compliledData[counter][2] = stateTrump.get(name);
             String funding = stateFunding.get(name);
-            funding = cleanUp(funding);
-            compliledData[counter][3] = stateFunding.get(funding);
+            System.out.println(funding);
+            String fundingName = cleanUp(funding);
+            compliledData[counter][3] = fundingName;
+            counter++;
         }
 
 
-        for (int i = 0; i < compliledData.length; i++) {
-            for (int j = 0; j < compliledData[0].length; j++) {
-                System.out.print(compliledData[i][j]);
-                System.out.print(", ");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < compliledData.length; i++) {
+//            for (int j = 0; j < compliledData[0].length; j++) {
+//                System.out.print(compliledData[i][j]);
+//                System.out.print(", ");
+//            }
+//            System.out.println();
+//        }
     }
 
     private static String cleanUp(String str) {
         str = str.replaceAll(" ","");
-        str = str.replace("$","");
+        str  = str.replaceAll("\\$","");
+        str = str.replaceAll("\"","");
         return str;
     }
 
--- src/Utils.java
+++ src/Utils.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public class Utils {
 
-    private static String csvRegx = ",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))";
+    private static String csvRegx =  ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
 
     public static String readFileAsString(String filepath) {
         StringBuilder output = new StringBuilder();
@@ -30,7 +30,7 @@ public class Utils {
 
 
 
-    public static HashMap<String, String> parseCSV(String csv_data, int firstLine, int firstIndex, int index) {
+    public static HashMap<String, String> parseCSV(String csv_data, int firstLine, int firstIndex, int index, int size) {
         String[] lines = csv_data.split(("\n"));
 
         HashMap<String, String> out = new HashMap<>();
@@ -38,196 +38,14 @@ public class Utils {
         Pattern csv = Pattern.compile(csvRegx);
         for (int i = firstLine; i < lines.length ; i++) {
             String[] temp = lines[i].split(csvRegx);
-            System.out.println(temp.length);
-            out.put(temp[firstIndex], temp[index]);
-        }
-        return out;
-    }
-
-    /*
-    public static ArrayList<State> initializeStates(String csv_data) {
-        String[] lines = csv_data.split(("\n"));
-        ArrayList<State> out = new ArrayList<>();
-
-        for (int i =1 ; i < lines.length ; i++) {
-            String current = lines[i];
-            State state = new State(stateName(current));
-
-            ArrayList<County> counties = initializeCounties("data/2016_Presidential_Results.csv");
-            for (County county : counties ) {
-                if (county.getState = state.getName()) state.addCounty(county);
-            }
-            out.add(current);
-        }
-        return out;
-    }
-
-    public static ArrayList<County> initializeCounties(String csv_data) {
-        String[] lines = csv_data.split(("\n"));
-        ArrayList<County> out = new ArrayList<>();
-
-        for (int i = 1; i < lines.length; i++) {
-            String current = lines[i];
-            County temp = new County();
-
-            String[] commaBreaks = current.split(",");
-            temp.setName(getName(commaBreaks));
-            temp.setFips(getFips(commaBreaks));
-            temp.setState(getState(commaBreaks));
-
-            ArrayList<Election2016> electionData = parseElection2016("data/2016_Presidential_Results.csv");
-            for (Election2016 data : electionData) {
-                if (data.getFips() == temp.getFips()) temp.setVote2016(data);
-            }
-            ArrayList<Education2016> educationData = parseEducation2016("data/Education.csv");
-            for (Education2016 data : educationData) {
-                if (data.getFips() == temp.getFips()) temp.setEducation2016(data);
-            }
-            ArrayList<Employment2016> employment2016 = parseEmployment2016("data/Employment.csv");
-            for (Employment2016 data : employment2016) {
-                if (data.getFips() == temp.getFips()) temp.setEmployment2016(data);
+            if (temp.length < size)
+                out.put("NULL", "NULL");
+            else{
+                out.put(temp[firstIndex], temp[index]);
             }
 
-            out.add(temp);
         }
         return out;
     }
 
-    public static ArrayList<Election2016> parseElection2016(String csv_data) {
-        String[] lines = csv_data.split(("\n"));
-        ArrayList<Election2016> out = new ArrayList<>();
-
-        for (int i = 1; i < lines.length; i++) {
-            String current = lines[i];
-            Election2016 temp = new Election2016();
-
-            String[] commaBreaks = current.split(",");
-            temp.setDemVotes(getVotesDem(commaBreaks));
-            temp.setGopVotes(getVotesGop(commaBreaks));
-            temp.setTotalVotes(getTotalVotes(commaBreaks));
-            temp.setFips(getCombinedFips(commaBreaks));
-
-            out.add(temp);
-        }
-        return out;
-    }
-
-    public static ArrayList<Education2016> parseEducation2016(String csv_data)  {
-        String[] lines = csv_data.split(("\n"));
-        ArrayList<Education2016> out = new ArrayList<>();
-
-        for (int i = 5; i < lines.length; i++) {
-            String current = lines[i];
-            Education2016 temp = new Education2016();
-
-            String[] commaBreaks = current.split(",");
-            temp.setFips(getEducationFips(commaBreaks));
-            temp.setNoHighSchool(getNoHighSchool(commaBreaks));
-            temp.setOnlyHighSchool(getOnlyHighSchool(commaBreaks));
-            temp.setSomeCollege(getSomeCollege(commaBreaks));
-            temp.setBachelorsOrMore(getBachs(commaBreaks));
-
-            out.add(temp);
-        }
-        return out;
-    }
-
-
-    public static ArrayList<Employment2016> parseEmployment2016(String csv_data) {
-        String[] lines = csv_data.split("\n");
-        ArrayList<Employment2016> out = new ArrayList<>();
-
-        for (int i = 9; i < lines.length; i++) {
-            String current = lines[i];
-            Employment2016 temp = new Employment2016();
-
-            String[] commaBreaks = current.split(",");
-            temp.setFips();
-            temp.setUnemployedPercent();
-            temp.setUnemployedLaborForce();
-            temp.setEmployedLaborForce();
-            temp.setTotalLaborForce();
-
-            out.add(temp);
-        }
-        return out;
-    }
-
-
-
-    private static int getEducationFips(String[] commaBreaks) { return Integer.parseInt(commaBreaks[0]); }
-
-    private static double getBachs(String[] commaBreaks) { return Integer.parseInt( commaBreaks[commaBreaks.length -1]); }
-
-    private static double getSomeCollege(String[] commaBreaks) { return Integer.parseInt( commaBreaks[commaBreaks.length - 2]); }
-
-    private static double getOnlyHighSchool(String[] commaBreaks) { return Integer.parseInt( commaBreaks[commaBreaks.length - 3]); }
-
-    private static double getNoHighSchool(String[] commaBreaks) { return Integer.parseInt( commaBreaks[commaBreaks.length -4]); }
-
-
-    private static int getCombinedFips(String[] commaBreaks) {
-        String temp = commaBreaks[commaBreaks.length - 1];
-        if (temp.length() == 4 ) return Integer.parseInt("0" + temp);
-        return Integer.parseInt(temp);
-    }
-
-
-    private static double getTotalVotes(String[] commaBreaks) { return Double.parseDouble(commaBreaks[3]); }
-
-    private static double getVotesGop(String[] commaBreaks) { return Double.parseDouble(commaBreaks[2]); }
-
-    private static double getVotesDem(String[] commaBreaks) { return Double.parseDouble(commaBreaks[1]); }
-
-    */
-
-//    private static int getDiff(String current) {
-//        String[] temp = current.split("\"");
-//        String diff = temp[1];
-//        if (diff.contains(",")) {
-//            return Integer.parseInt(diff.replace(",", ""));
-//        } else return Integer.parseInt(diff);
-//    }
-
-//        public static ArrayList<ElectionResult> parse2016PresidentialResults(String csv_data) {
-//        String[] lines = csv_data.split("\n");
-//        ArrayList<ElectionResult> out = new ArrayList<>();
-//
-//        for (int i = 1; i < lines.length; i++) {
-//            String current = lines[i];
-//            ElectionResult temp = new ElectionResult();
-//            temp.setDiff(getDiff(current));
-//
-//            String[] commaBreaks = current.split(",");
-//            temp.setVotesDem(getVotesDem(commaBreaks));
-//            temp.setVotesGOP(getVotesGop(commaBreaks));
-//            temp.setTotalVotes(getTotalVotes(commaBreaks));
-//            temp.setPerDem((getPerDem(commaBreaks)));
-//            temp.setPerGOP((getPerGop(commaBreaks)));
-//            temp.setPerPointDiff((getPerPointDiff(commaBreaks)));
-//            temp.setStateAbbr((getStateAbbr(commaBreaks)));
-//            temp.setCountyName((getCountyName(commaBreaks)));
-//            temp.setCombinedFips((getCombinedFips(commaBreaks)));
-//
-//            out.add(temp);
-//        }
-//
-//        return out;
-//    }
-
-
-//
-//    private static String getCountyName(String[] commaBreaks) { return commaBreaks[commaBreaks.length - 2]; }
-//
-//    private static String getStateAbbr(String[] commaBreaks) { return commaBreaks[commaBreaks.length -3];}
-//
-//    private static double getPerPointDiff(String[] commaBreaks) {
-//        String temp = commaBreaks[commaBreaks.length - 4];
-//        temp = temp.substring(0,temp.length() - 1);
-//        return Double.parseDouble(temp);
-//    }
-//
-//    private static double getPerGop(String[] commaBreaks) { return Double.parseDouble(commaBreaks[5]); }
-//
-//    private static double getPerDem(String[] commaBreaks) { return Double.parseDouble(commaBreaks[4]); }
 }
